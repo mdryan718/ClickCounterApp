e704d1bccaeca254e2721a6a5d7f8301572c9c74
Meghan Ryan <ryan.meghan@outlook.com>
2020-01-06 11:35:32 -0500

    Add button to UI

--- ViewController.swift
+++ ViewController.swift
@@ -17,7 +17,12 @@ class ViewController: UIViewController {
         label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
         label.text = "0"
         view.addSubview(label)
-        // Do any additional setup after loading the view.
+        // button
+        let button = UIButton()
+        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
     }
 
 
