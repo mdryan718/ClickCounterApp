b54cf5490984ec8a252fc3ac6cd1af51d92328d3
Meghan Ryan <ryan.meghan@outlook.com>
2020-01-06 12:23:49 -0500

    Add additional increment label

--- ViewController.swift
+++ ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     var count = 0
     var label: UILabel!
+    var secondLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,9 +22,16 @@ class ViewController: UIViewController {
         label.text = "0"
         view.addSubview(label)
         self.label = label
+        
+        // second label
+              let secondLabel = UILabel()
+              secondLabel.frame = CGRect(x: 150, y: 200, width: 60, height: 60)
+              secondLabel.text = "0"
+              view.addSubview(secondLabel)
+              self.secondLabel = secondLabel
         // button
         let button = UIButton()
-        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
+        button.frame = CGRect(x: 150, y: 350, width: 60, height: 60)
         button.setTitle("Click", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         view.addSubview(button)
@@ -34,6 +42,7 @@ class ViewController: UIViewController {
     @objc func incrementCount() {
         self.count += 1
         self.label.text = "\(self.count)"
+        self.secondLabel.text = "\(self.count)"
     }
 
 
